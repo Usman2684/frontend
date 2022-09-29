0b06fd309dc83b0e4f62c071c50d0562a4ca229f
ziaullah667773 <ziaullah667773@gmail.com>
2022-09-29 15:22:28 +0500

    all components added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,47 @@
 
 const Navbar = () => (
-    <>
-          <nav className="nav">
-            <h1>The <span>Planner</span></h1>
-             <ul className="nav-items">
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Projects</li>
-                <li>Pricing</li>
-                <li>Contact</li>
-                <li>Blogs</li>
-             </ul>
-            </nav>
-    </>
-);
 
-export default Navbar;
+  <header class="header">
+
+    <a href="#" class="logo">The<span>Planner</span></a>
+
+    <nav class="navbar">
+      <a href="#home">home</a>
+      <a href="#about">about</a>
+      <a href="#services">services</a>
+      <a href="#projects">projects</a>
+      <a href="#pricing">pricing</a>
+      <a href="#contact">contact</a>
+      <a href="#blogs">blogs</a>
+    </nav>
+{/* 
+    <div class="icons">
+      <div id="menu-btn" class="fas fa-bars"></div>
+      <div id="info-btn" class="fas fa-info-circle"></div>
+      <div id="search-btn" class="fas fa-search"></div>
+      <div id="login-btn" class="fas fa-user"></div>
+    </div>
+
+    <form action="" class="search-form">
+      <input type="search" name="" placeholder="search here..." id="search-box" />
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+
+    <form action="" class="login-form">
+      <h3>login form</h3>
+      <input type="email" placeholder="enter your email" class="box" />
+        <input type="password" placeholder="enter your password" class="box" />
+          <div class="flex">
+            <input type="checkbox" name="" id="remember-me" />
+              <label for="remember-me">remember me</label>
+              <a href="#">forgot password?</a>
+          </div>
+          <input type="submit" value="login now" class="btn" />
+            <p>don't have an account <a href="#">create one!</a></p>
+          </form> */}
+
+        </header>
+
+        );
+
+        export default Navbar;
