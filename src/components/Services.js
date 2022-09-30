ac6a4399a8144ab9681a2f6564f2c40f29c9976e
Usman <usmanw2684@gmail.com>
2022-09-30 22:54:35 +0500

    fixed

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,49 +8,49 @@ const services = ()=>(
     <div class="box-container">
  
        <div class="box">
-          <Image src="/images/service-1.png" width={100} height={100}  />
+          <Image src="/images/service-1.png" width={100} height={100} alt=""   />
           <h3>building construction</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-2.png" width={100} height={100}/>
+          <Image src="/images/service-2.png" width={100} height={100} alt="" />
           <h3>house renovation</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-3.png" width={100} height={100}/>
+          <Image src="/images/service-3.png" width={100} height={100} alt="" />
           <h3>architechture design</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-4.png" width={100} height={100}/>
+          <Image src="/images/service-4.png" width={100} height={100} alt="" />
           <h3>material supply</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-5.png" width={100} height={100}/>
+          <Image src="/images/service-5.png" width={100} height={100} alt="" />
           <h3>construction consultant</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-6.png" width={100} height={100}/>
+          <Image src="/images/service-6.png" width={100} height={100} alt="" />
           <h3>interior design</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-7.png" width={100} height={100}/>
+          <Image src="/images/service-7.png" width={100} height={100} alt="" />
           <h3>building maintenance</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
  
        <div class="box">
-          <Image src="/images/service-8.png" width={100} height={100}/>
+          <Image src="/images/service-8.png" width={100} height={100} alt="" />
           <h3>building renovation</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, necessitatibus.</p>
        </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,21 +14,7 @@ const App = ({ Component, pageProps }) => {
             <Head>
                 <title>The Planer</title>
                 <meta name="viewport" content="width=devide-width, intial-scale=1" />
-                {/* <link 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" 
-                    rel="stylesheet" 
-                    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" 
-                    crossorigin="anonymous"
-                ></link>*/}
-
-                {/* <script 
-                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
-                    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" 
-                    crossorigin="anonymous"
-                ></script>  */}
-               <script src="/js/script.js"></script>
-                
-
+               {/* <script src="/js/script.js"></script> */}
             </Head>
             <Component {...pageProps} />
         </Provider>
